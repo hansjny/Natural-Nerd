72d0f69b3f5eddc04fd6aea1d68d8bbc759b0c6c
Hans Jørgen Nygårdshaug <hans.nyg@gmail.com>
2017-07-20 23:39:41 +0200

    Update testled.cc

--- testled.cc
+++ testled.cc
@@ -7,6 +7,7 @@ CRGB leds[NUM_LEDS];
 
 void setup() { 
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  int i = 0;
     for (i = 0; i < NUM_LEDS; i++) 
       leds[i] = CRGB(255, 0, 0);
     FastLED.show(); 
